54fbc304cef4d29d4a1db2ad22b30b7ea9c9db46
wangzhen <wangzhen617@jd.com>
2021-02-07 18:56:29 +0800

    added two nav-items of dropdown form

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -25,40 +25,31 @@ class HeaderComponent extends React.Component{
               组织管理
               </Link>
             </li>
-            <li className="nav-item">
-            <Link to="/resourcelist" className="nav-link font-weight-bold text-white">            
-              资源管理
-            </Link>
+            <li className="nav-item dropdown show">
+            <button className="btn nav-item dropdown-toggle text-white font-weight-bold" id="bd-versions" data-toggle="dropdown" >资源管理</button>
+            <div className="dropdown-menu show" aria-labelledby="bd-versions">
+              <Link to="/resourcelist" className="dropdown-item">资源列表</Link>
+              <Link to="/iotmenu" className="dropdown-item">物管平台菜单</Link>
+              <Link to="/iotapi" className="dropdown-item">物管平台API</Link>
+              <Link to="/emenu" className="dropdown-item">能源平台菜单</Link>
+              <Link to="/eapi" className="dropdown-item">能源平台API</Link>
+              <Link to="/govmenu" className="dropdown-item">能源平台政府侧菜单</Link>
+              <Link to="/govapi" className="dropdown-item">能源平台政府侧API</Link>
+            </div>
             </li>
-            <li className="nav-item">
-            <Link to="/iotmenu" className="nav-link font-weight-bold text-white">            
-              iotmenu
-            </Link>
+            <li className="nav-item dropdown show">
+            <button className="btn nav-item dropdown-toggle text-white font-weight-bold" id="bd-versions" data-toggle="dropdown" >角色管理</button>
+            <div className="dropdown-menu show" aria-labelledby="bd-versions">
+              <Link to="/rolelist" className="dropdown-item">角色列表</Link>
+              <Link to="/orgrolelist" className="dropdown-item">组织角色列表</Link>
+              <Link to="/bizrolelist" className="dropdown-item">业务角色列表</Link>
+            </div>
             </li>
-            <li className="nav-item">
-            <Link to="/iotapi" className="nav-link font-weight-bold text-white">            
-              iotapi
-            </Link>
+            <li className="nav-item font-weight-bold">
+              <Link to="/authlist" className="text-white nav-link">权限管理</Link>
             </li>
-            <li className="nav-item">
-            <Link to="/emenu" className="nav-link font-weight-bold text-white">            
-              emenu
-            </Link>
-            </li>
-            <li className="nav-item">
-            <Link to="/eapi" className="nav-link font-weight-bold text-white">            
-              eapi
-            </Link>
-            </li>
-            <li className="nav-item">
-            <Link to="/govmenu" className="nav-link font-weight-bold text-white">            
-              govmenu
-            </Link>
-            </li>
-            <li className="nav-item">
-            <Link to="/govapi" className="nav-link font-weight-bold text-white">            
-              govapi
-            </Link>
+            <li className="nav-item font-weight-bold">
+              <Link to="/tenantlist" className="text-white nav-link">租户管理</Link>
             </li>
           </ul>
         </nav>
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 
 class HomePage extends React.Component{
     constructor(props){
@@ -11,37 +10,6 @@ class HomePage extends React.Component{
     render(){
         return(
             <div>
-                <div>
-                    <ul>
-                    <li>
-                    <Link to="/rolelist">            
-                    <h2>角色列表</h2>
-                    </Link>
-                    </li>
-                    <li>
-                    <Link to="/orgrolelist">            
-                    <h2 >组织角色列表</h2>
-                    </Link>
-                    </li>
-                    <li>
-                    <Link to="/bizrolelist">            
-                    <h2 >业务角色列表</h2>
-                    </Link>
-                    </li>
-                    <br></br>
-                    <li>
-                    <Link to="/authlist">            
-                    <h2 >权限列表</h2>
-                    </Link>
-                    </li>
-                    <br></br>
-                    <li>
-                    <Link to="/tenantlist">            
-                    <h2 >租户列表</h2>
-                    </Link>
-                    </li>
-                    </ul>
-                </div>
             </div>
         )
     }
