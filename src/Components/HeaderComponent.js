c354073f37f526974fc584daf918d8cd94a1ebbb
wangzhen <wangzhen617@jd.com>
2021-02-23 14:49:45 +0800

    fixed dropdown problem...

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 class HeaderComponent extends React.Component{
    constructor(props){
@@ -8,6 +8,7 @@ class HeaderComponent extends React.Component{
 
         }
    }
+   
    render(){
        return(
         <nav className="navbar navbar-expand-lg navbar-light color-nav">
@@ -25,9 +26,10 @@ class HeaderComponent extends React.Component{
               组织管理
               </Link>
             </li>
-            <li className="nav-item dropdown show">
-            <button className="btn nav-item dropdown-toggle text-white font-weight-bold" id="bd-versions" data-toggle="dropdown" >资源管理</button>
-            <div className="dropdown-menu show color-dd1" aria-labelledby="bd-versions">
+            <li className="nav-item dropdown">
+            <button className="btn nav-item dropdown-toggle text-white font-weight-bold" id="dropdown1" data-toggle="dropdown" aria-haspopup="true" 
+                 aria-expanded="false">资源管理</button>
+            <div className="dropdown-menu color-dd1" aria-labelledby="dropdown1">
               <Link to="/resourcelist" className="dropdown-item text-white font-weight-bold">资源列表</Link>
               <Link to="/iotmenu" className="dropdown-item text-white font-weight-bold">物管平台菜单</Link>
               <Link to="/iotapi" className="dropdown-item text-white font-weight-bold">物管平台API</Link>
@@ -37,9 +39,10 @@ class HeaderComponent extends React.Component{
               <Link to="/govapi" className="dropdown-item text-white font-weight-bold">能源平台政府侧API</Link>
             </div>
             </li>
-            <li className="nav-item dropdown show">
-            <button className="btn nav-item dropdown-toggle text-white font-weight-bold" id="bd-versions" data-bs-toggle="dropdown">角色管理</button>
-            <div className="dropdown-menu dropdown-menu-end show color-dd2"  aria-labelledby="bd-versions">
+            <li className="nav-item dropdown">
+            <button type="button" className="btn dropdown-toggle text-white font-weight-bold" id="dropdown2" data-toggle="dropdown" aria-haspopup="true" 
+                 aria-expanded="false">角色管理</button>
+            <div className="dropdown-menu color-dd1" aria-labelledby="dropdown2">
               <Link to="/rolelist" className="dropdown-item text-white font-weight-bold">角色列表</Link>
               <Link to="/orgrolelist" className="dropdown-item text-white font-weight-bold">组织角色列表</Link>
               <Link to="/bizrolelist" className="dropdown-item text-white font-weight-bold">业务角色列表</Link>
